0d94c0968c1cfbd0b15419495ff2d3c2728aa3d8
Marius <m@mari.us.com>
2022-01-08 18:42:48 +0100

    Bump version

--- freenom.sh
+++ freenom.sh
@@ -12,7 +12,7 @@
 # This is free software, and you are welcome to redistribute it               #
 # under certain conditions.                                                   #
 # See LICENSE file for more information                                       #
-# gpl-3.0-only                                                  v2021-12-07   #
+# gpl-3.0-only                                                  v2022-01-08   #
 ###############################################################################
 
 ########
